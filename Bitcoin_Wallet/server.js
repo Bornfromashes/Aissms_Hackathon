b10b9fe491c8b79dba7bc2c2983150be7f64e038
Bornfromashes <amit220698@gmail.com>
2018-01-28 22:00:15 +0530

    adding wallet functionalities

--- Bitcoin_Wallet/server.js
+++ Bitcoin_Wallet/server.js
@@ -5,11 +5,18 @@ const http= require('http');
 const path= require('path');
 const ejs= require('ejs');
 const bodyParser= require('body-parser');
+const bitcore= require('bitcore-lib')
 const methodOverride= require('method-override');
 var app=express();
 var stringify = require('json-stringify-safe');
 global.fetch = require('node-fetch')
 const cc = require('cryptocompare')
+
+var accountSid = 'AC7dd0ed7e66f3cf5be43c7e855be66bc7'; // Your Account SID from www.twilio.com/console
+var authToken = 'cf3fe2fe77b0767c776e729786900f98'; // Your Auth Token from www.twilio.com/console
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
 
 app.use(methodOverride());
 app.use(bodyParser.json());
@@ -21,7 +28,7 @@ app.set( 'views', path.join( __dirname, 'public/site/production' ));
 
 app.use(express.static(path.join('static')));
 
-const db= 'mongodb://qwe:qwe@ds129333.mlab.com:29333/abc';
+const db= 'mongodb://qwe:qwe@ds129143.mlab.com:29143/try';
 mongoose.Promise= global.Promise;
 
 mongoose.connect(db, function(err){
@@ -30,11 +37,13 @@ mongoose.connect(db, function(err){
   }
 });
 
-var videoSchema= new mongoose.Schema({
-  uid: String,
-  dis: String,
+var formSchema= new mongoose.Schema({
+  Name: String,
+  Address: String,
+  PK: String,
+  Email: String
 });
-const Video= mongoose.model('video', videoSchema, 'meter');
+const Complain= mongoose.model('complain', formSchema, 'bit');
 
 app.get('/tables.html', function(req, res){
 Video.find({}, function(err, videos){
@@ -47,6 +56,28 @@ Video.find({}, function(err, videos){
    }
   });
 });
+app.get('/signup', function(req, res){
+  console.log('signup');
+  res.render('signup.html');
+});
+app.post('/signup', function(req, res){	
+  var brainsrc= req.body.brainsrc;
+  var savedata= new Complain(req.body);
+  savedata.save(function(err, result) {
+		if (err) throw err;
+
+		if(result) {
+			res.json(result);
+		}
+	})
+  console.log(brainsrc);
+  var input= new Buffer(brainsrc);
+  var hash= bitcore.crypto.Hash.sha256(input)
+  var bn= bitcore.crypto.BN.fromBuffer(hash)
+  var pk= new bitcore.PrivateKey(bn).toWIF();
+  var addy= new bitcore.PrivateKey(bn).toAddress();
+  res.send("The Brain wallet of: " + brainsrc +"<br>Addy:" + addy + "<br>Private Key:" + pk);
+});	
 app.get('/index.html', function(req, res){
   console.log('home');
   cc.price('BTC', ['USD', 'EUR', 'INR'])
@@ -55,6 +86,27 @@ app.get('/index.html', function(req, res){
 })
 .catch(console.error)
 });
+
+app.get('/OTP', function(req, res){
+  console.log('OTP');
+  res.render('OTP.html');
+});
+
+app.post('/OTP', function(req, res){
+ var no= req.body.Phone;	
+ client.messages.create({
+    body: '22340',
+    to: '+917507934548',  // Text this number
+    from: '+12028497516' // From a valid Twilio number
+})
+.then((message) => console.log(message.sid));
+if (no== req.body.OTP)
+ {
+ 	res.redirect('index.html');
+ }
+});
+
+
 app.get('/profile.html', function(req, res){
   console.log('profile');
   res.render('profile.html');
